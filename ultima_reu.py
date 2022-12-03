1047e0ece12130888d9c7e673d942ff1cc8f807b
Andres Gomez <103458191+DanersG@users.noreply.github.com>
2022-12-03 09:42:49 -0600

    Added main

--- ultima_reu.py
+++ ultima_reu.py
@@ -5,7 +5,8 @@
 ###
 ###
 class Budget(object):
-  def __init__(self, expense=0, income=0) -> None:
+  def __init__(self, expense=0, income=0, budget=0) -> None:
+    self.budget =0.0
     self.expense = 0
     self.income = 0
     self.total_money = self.income - self.expense
@@ -15,4 +16,10 @@ class Budget(object):
       "credit": [],
       "debit": []
     }
-
+    
+def main():
+    budget = float(input("Enter new budget"))
+    newBudget = Budget(0,0, budget)
+    
+if __name__ == '__main__':
+  main()
