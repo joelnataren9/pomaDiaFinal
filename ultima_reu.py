b891e96b4af6bb1590c42b8626f55c16b1657348
joelnataren9 <joelnataren9@hotmail.com>
2022-12-03 10:49:04 -0500

    Modifique el init y str functions

--- ultima_reu.py
+++ ultima_reu.py
@@ -6,8 +6,8 @@
 ###
 class Budget(object):
   def __init__(self, expense=0, income=0) -> None:
-    self.expense = 0
-    self.income = 0
+    self.expense = float(expense)
+    self.income = float(income)
     self.total_money = self.income - self.expense
     self.expense_types = {}
     self.income_types = {}
@@ -17,5 +17,9 @@ class Budget(object):
     }
 
   def __str__(self):
-    out_str = "Total Money so far {}\n\n Income: {}\n Expenses: {}".format(self.total_money, self.income, self.expense)
+    out_str = "Total Money so far ${:.2f}\n\n Income: ${:.2f}\n Expenses: ${:.2f}".format(self.total_money, self.income, self.expense)
     return out_str
+
+
+p = Budget(10, 100)
+print(p)
