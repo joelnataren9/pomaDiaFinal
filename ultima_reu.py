74721b37dc8a6e157b3d2025b9266f686870df9f
joelnataren9 <joelnataren9@hotmail.com>
2022-12-03 10:28:25 -0500

    Inicializado la clase BUdget

--- ultima_reu.py
+++ ultima_reu.py
@@ -4,7 +4,15 @@
 ###
 ###
 ###
-class MyMathEquations(object):
-  pass
-
+class Budget(object):
+  def __init__(self, expense=0, income=0) -> None:
+    self.expense = 0
+    self.income = 0
+    self.total_money = self.income - self.expense
+    self.expense_types = {}
+    self.income_types = {}
+    self.payment_types = {
+      "credit": [],
+      "debit": []
+    }
 
